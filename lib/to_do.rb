6b06d19b4d79c9971fdff0529448e05782b3c807
Mohamed Warsame and Trevor Fast <mohamed@>
2017-04-25 08:30:46 -0700

    run first test

---
+++ lib/to_do.rb
@@ -0,0 +1,10 @@
+class Task
+  define_method(:initialize) do |description|
+    @description = description
+  end
+
+  define_method(:description) do
+    @description
+  end
+
+end
---
+++ spec/to_do_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('to_do')
+
+describe(Task) do
+  describe('#description') do
+    it("lets you give it a description") do
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+end
