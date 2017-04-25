f9d0ecf15e029060c9e9277452f673d18dc76726
Mohamed Warsame and Trevor Fast <mohamed@>
2017-04-25 09:03:54 -0700

    add tests and specs

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -8,4 +8,29 @@ describe(Task) do
       expect(test_task.description()).to(eq("scrub the zebra"))
     end
   end
+  describe('.all') do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe(".clear") do
+    it('empties out all of the saved tasks') do
+      Task.new("wash the lion").save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe(Task) do
+    before() do
+      Task.clear()
+    end
+  end
 end
